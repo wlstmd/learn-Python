81051af540d8cc4f855873a35ff0e7482ce94d90
유진승 <jinseung0327@gmail.com>
2023-11-27 23:34:50 +0900

    FEAT: std python

---
+++ 4.step.py
@@ -0,0 +1,16 @@
+distance = 250
+
+if distance >= 200:
+    print("눈 던지기 ")
+else:
+    print("눈사람 만들기")
+
+
+# 조건문 응용하기
+money = 2000
+if money >= 20000:
+    print("치킨과 콜라를 먹는다")
+elif money >= 10000:
+    print("떡볶이를 먹는다")
+else:
+    print("편의점에가서 김밥을 먹는다")
